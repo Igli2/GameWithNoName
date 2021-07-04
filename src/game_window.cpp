1e2395ce0521d334d3b6d5215e8503f0c1b0b805
Igli2 <sg.gerdon@gmx.de>
2021-07-04 22:22:32 +0200

    fixed game_window changes

--- src/game_window.cpp
+++ src/game_window.cpp
@@ -1,15 +1,12 @@
 #include "game_window.h"
 
 game_window::game_window(const std::string& title, const size_t width, const size_t height, const bool resizable, event::event_handler& ev_handler)
-    : render_window{title, width, height, resizable, ev_handler}, off_x{0} {
+    : render_window{title, width, height, resizable, ev_handler}, off_x{0}, resource_menu{(int)width, (int)height} {
     // button test
     Button button{10.0f, 10.0f, 100.0f, 80.0f};
     this->add_widget(std::move(button));
 
     // set initial resource menu size
-    int w, h;
-    this->get_window_size(&w, &h);
-    this->resource_menu.updateWindowSize(w, h);
     this->resource_menu.addResources(Resource::BEECH_LOG, 10);
     this->resource_menu.removeResources(Resource::BEECH_LOG, 2);
 }
